60cd5b38819a1b24279dcbd98d617391503d0ffa
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-01-02 08:18:54 -0500

    remove duplicate methods

--- src/internal/core.ts
+++ src/internal/core.ts
@@ -495,7 +495,7 @@ class FiberImpl<in out A = any, in out E = any> implements Fiber.Fiber<A, E> {
 
   runLoop(effect: Primitive): Exit.Exit<A, E> | Yield {
     const prevFiber = (globalThis as any)[currentFiberUri]
-    ;(globalThis as any)[currentFiberUri] = this
+      ; (globalThis as any)[currentFiberUri] = this
     let yielding = false
     let current: Primitive | Yield = effect
     this.currentOpCount = 0
@@ -528,7 +528,7 @@ class FiberImpl<in out A = any, in out E = any> implements Fiber.Fiber<A, E> {
       }
       return exitDie(error)
     } finally {
-      ;(globalThis as any)[currentFiberUri] = prevFiber
+      ; (globalThis as any)[currentFiberUri] = prevFiber
     }
   }
 
@@ -536,8 +536,7 @@ class FiberImpl<in out A = any, in out E = any> implements Fiber.Fiber<A, E> {
     symbol: S
   ):
     | (Primitive & Record<S, (value: any, fiber: FiberImpl) => Primitive>)
-    | undefined
-  {
+    | undefined {
     while (true) {
       const op = this._stack.pop()
       if (!op) return undefined
@@ -626,18 +625,18 @@ export const fiberInterruptAll = <A extends Iterable<Fiber.Fiber<any, any>>>(
 export interface Primitive {
   readonly [identifier]: string
   readonly [successCont]:
-    | ((value: unknown, fiber: FiberImpl) => Primitive | Yield)
-    | undefined
+  | ((value: unknown, fiber: FiberImpl) => Primitive | Yield)
+  | undefined
   readonly [failureCont]:
-    | ((cause: Cause.Cause<unknown>, fiber: FiberImpl) => Primitive | Yield)
-    | undefined
+  | ((cause: Cause.Cause<unknown>, fiber: FiberImpl) => Primitive | Yield)
+  | undefined
   readonly [ensureCont]:
-    | ((
-      fiber: FiberImpl
-    ) =>
-      | ((value: unknown, fiber: FiberImpl) => Primitive | Yield)
-      | undefined)
-    | undefined
+  | ((
+    fiber: FiberImpl
+  ) =>
+    | ((value: unknown, fiber: FiberImpl) => Primitive | Yield)
+    | undefined)
+  | undefined
   [evaluate](fiber: FiberImpl): Primitive | Yield
 }
 
@@ -1013,11 +1012,11 @@ export const gen = <
 ): Effect.Effect<
   AEff,
   [Eff] extends [never] ? never
-    : [Eff] extends [YieldWrap<Effect.Effect<infer _A, infer E, infer _R>>] ? E
-    : never,
+  : [Eff] extends [YieldWrap<Effect.Effect<infer _A, infer E, infer _R>>] ? E
+  : never,
   [Eff] extends [never] ? never
-    : [Eff] extends [YieldWrap<Effect.Effect<infer _A, infer _E, infer R>>] ? R
-    : never
+  : [Eff] extends [YieldWrap<Effect.Effect<infer _A, infer _E, infer R>>] ? R
+  : never
 > =>
   suspend(() =>
     unsafeFromIterator(
@@ -1098,13 +1097,13 @@ export const andThen: {
   ): <E, R>(
     self: Effect.Effect<A, E, R>
   ) => [X] extends [Effect.Effect<infer A1, infer E1, infer R1>] ? Effect.Effect<A1, E | E1, R | R1>
-    : Effect.Effect<X, E, R>
+      : Effect.Effect<X, E, R>
   <X>(
     f: NotFunction<X>
   ): <A, E, R>(
     self: Effect.Effect<A, E, R>
   ) => [X] extends [Effect.Effect<infer A1, infer E1, infer R1>] ? Effect.Effect<A1, E | E1, R | R1>
-    : Effect.Effect<X, E, R>
+      : Effect.Effect<X, E, R>
   <A, E, R, X>(
     self: Effect.Effect<A, E, R>,
     f: (a: A) => X
@@ -1134,13 +1133,13 @@ export const tap: {
   ): <E, R>(
     self: Effect.Effect<A, E, R>
   ) => [X] extends [Effect.Effect<infer _A1, infer E1, infer R1>] ? Effect.Effect<A, E | E1, R | R1>
-    : Effect.Effect<A, E, R>
+      : Effect.Effect<A, E, R>
   <X>(
     f: NotFunction<X>
   ): <A, E, R>(
     self: Effect.Effect<A, E, R>
   ) => [X] extends [Effect.Effect<infer _A1, infer E1, infer R1>] ? Effect.Effect<A, E | E1, R | R1>
-    : Effect.Effect<A, E, R>
+      : Effect.Effect<A, E, R>
   <A, E, R, X>(
     self: Effect.Effect<A, E, R>,
     f: (a: NoInfer<A>) => X
@@ -2661,7 +2660,7 @@ export const timeoutOrElse: {
       {
         onWinner: ({ fiber, index, parentFiber }) => {
           if (index !== 0) return
-          ;(parentFiber as FiberImpl).context = fiber.context
+            ; (parentFiber as FiberImpl).context = fiber.context
         }
       }
     )
@@ -3022,8 +3021,8 @@ export const uninterruptibleMask = <A, E, R>(
 /** @internal */
 export const all = <
   const Arg extends
-    | Iterable<Effect.Effect<any, any, any>>
-    | Record<string, Effect.Effect<any, any, any>>,
+  | Iterable<Effect.Effect<any, any, any>>
+  | Record<string, Effect.Effect<any, any, any>>,
   O extends {
     readonly concurrency?: Concurrency | undefined
     readonly discard?: boolean | undefined
@@ -3468,7 +3467,7 @@ class Semaphore {
   public waiters = new Set<() => void>()
   public taken = 0
 
-  constructor(readonly permits: number) {}
+  constructor(readonly permits: number) { }
 
   get free() {
     return this.permits - this.taken
@@ -3543,7 +3542,7 @@ const succeedFalse = succeed(false)
 class Latch implements Effect.Latch {
   waiters: Array<(_: Effect.Effect<void>) => void> = []
   scheduled = false
-  constructor(private isOpen: boolean) {}
+  constructor(private isOpen: boolean) { }
 
   private unsafeSchedule(fiber: Fiber.Fiber<boolean>) {
     if (this.scheduled || this.waiters.length === 0) {
@@ -3651,9 +3650,9 @@ const NoopSpanProto: Omit<Tracer.Span, "parent" | "name" | "context"> = {
   attributes: new Map(),
   links: [],
   kind: "internal",
-  attribute() {},
-  event() {},
-  end() {}
+  attribute() { },
+  event() { },
+  end() { }
 }
 
 /** @internal */
@@ -3684,8 +3683,8 @@ export const unsafeMakeSpan = <XA, XE>(
   const parent = options.parent
     ? Option.some(options.parent)
     : options.root
-    ? Option.none()
-    : filterDisablePropagation(InternalContext.getOption(fiber.context, Tracer.ParentSpan))
+      ? Option.none()
+      : filterDisablePropagation(InternalContext.getOption(fiber.context, Tracer.ParentSpan))
 
   let span: Tracer.Span
 
@@ -3843,14 +3842,14 @@ export const useSpan: {
     evaluate: (span: Tracer.Span) => Effect.Effect<A, E, R>
   ]
 ) => {
-  const options = addSpanStackTrace(args.length === 1 ? undefined : args[0])
-  const evaluate: (span: Tracer.Span) => Effect.Effect<A, E, R> = args[args.length - 1]
-  return withFiberUnknown((fiber) => {
-    const span = unsafeMakeSpan(fiber, name, options)
-    const clock = fiber.getRef(CurrentClock)
-    return onExit(evaluate(span), (exit) => endSpan(span, exit, clock))
-  })
-}
+    const options = addSpanStackTrace(args.length === 1 ? undefined : args[0])
+    const evaluate: (span: Tracer.Span) => Effect.Effect<A, E, R> = args[args.length - 1]
+    return withFiberUnknown((fiber) => {
+      const span = unsafeMakeSpan(fiber, name, options)
+      const clock = fiber.getRef(CurrentClock)
+      return onExit(evaluate(span), (exit) => endSpan(span, exit, clock))
+    })
+  }
 
 /** @internal */
 export const withParentSpan: {
@@ -3908,19 +3907,19 @@ export const annotateCurrentSpan: {
   (key: string, value: unknown): Effect.Effect<void>
   (values: Record<string, unknown>): Effect.Effect<void>
 } = (...args: [Record<string, unknown>] | [key: string, value: unknown]) =>
-  withFiber((fiber) => {
-    const span = InternalContext.getOption(fiber.context, Tracer.ParentSpan)
-    if (span._tag === "Some" && span.value._tag === "Span") {
-      if (args.length === 1) {
-        for (const [key, value] of Object.entries(args[0])) {
-          span.value.attribute(key, value)
+    withFiber((fiber) => {
+      const span = InternalContext.getOption(fiber.context, Tracer.ParentSpan)
+      if (span._tag === "Some" && span.value._tag === "Span") {
+        if (args.length === 1) {
+          for (const [key, value] of Object.entries(args[0])) {
+            span.value.attribute(key, value)
+          }
+        } else {
+          span.value.attribute(args[0], args[1])
         }
-      } else {
-        span.value.attribute(args[0], args[1])
       }
-    }
-    return void_
-  })
+      return void_
+    })
 
 /** @internal */
 export const currentSpan: Effect.Effect<Tracer.Span, Cause.NoSuchElementError> = withFiber((fiber) => {
@@ -4007,11 +4006,11 @@ export const currentTimeNanos: Effect.Effect<bigint> = clockWith((clock) => cloc
 // ----------------------------------------------------------------------------
 
 /** @internal */
-export const YieldableError: new(
+export const YieldableError: new (
   message?: string,
   options?: ErrorOptions
 ) => Cause.YieldableError = (function() {
-  class YieldableError extends globalThis.Error {}
+  class YieldableError extends globalThis.Error { }
   Object.assign(YieldableError.prototype, EffectProto, StructuralPrototype, {
     [identifier]: "Failure",
     [evaluate]() {
@@ -4035,7 +4034,7 @@ export const YieldableError: new(
 })()
 
 /** @internal */
-export const Error: new<A extends Record<string, any> = {}>(
+export const Error: new <A extends Record<string, any> = {}>(
   args: Equals<A, {}> extends true ? void : { readonly [P in keyof A]: A[P] }
 ) => Cause.YieldableError & Readonly<A> = (function() {
   const plainArgsSymbol = Symbol.for("effect/Data/Error/plainArgs")
@@ -4059,14 +4058,14 @@ export const Error: new<A extends Record<string, any> = {}>(
 /** @internal */
 export const TaggedError = <Tag extends string>(
   tag: Tag
-): new<A extends Record<string, any> = {}>(
+): new <A extends Record<string, any> = {}>(
   args: Equals<A, {}> extends true ? void
     : { readonly [P in keyof A as P extends "_tag" ? never : P]: A[P] }
 ) => Cause.YieldableError & { readonly _tag: Tag } & Readonly<A> => {
   class Base extends Error<{}> {
     readonly _tag = tag
   }
-  ;(Base.prototype as any).name = tag
+  ; (Base.prototype as any).name = tag
   return Base as any
 }
 
@@ -4417,71 +4416,50 @@ export const defaultLogger = loggerWithConsoleLog(stringLogger)
 
 /** @internal */
 export const logWithLevel = (level?: LogLevel.LogLevel) =>
-(
-  ...message: ReadonlyArray<any>
-): Effect.Effect<void> => {
-  let cause: Cause.Cause<unknown> | undefined = undefined
-  for (let i = 0, len = message.length; i < len; i++) {
-    const msg = message[i]
-    if (isCause(msg)) {
-      if (cause !== undefined) {
-        cause = causeFromFailures(cause.failures.concat(msg.failures))
-      } else {
-        cause = msg
+  (
+    ...message: ReadonlyArray<any>
+  ): Effect.Effect<void> => {
+    let cause: Cause.Cause<unknown> | undefined = undefined
+    for (let i = 0, len = message.length; i < len; i++) {
+      const msg = message[i]
+      if (isCause(msg)) {
+        if (cause !== undefined) {
+          cause = causeFromFailures(cause.failures.concat(msg.failures))
+        } else {
+          cause = msg
+        }
+        message = [...message.slice(0, i), ...message.slice(i + 1)]
+        i--
       }
-      message = [...message.slice(0, i), ...message.slice(i + 1)]
-      i--
     }
-  }
-  if (cause === undefined) {
-    cause = causeFromFailures([])
-  }
-  return withFiber((fiber) => {
-    const clock = fiber.getRef(CurrentClock)
-    const loggers = fiber.getRef(CurrentLoggers)
-    const annotations = fiber.getRef(CurrentLogAnnotations)
-    const logLevel = level ?? fiber.getRef(CurrentLogLevel)
-    const spans = fiber.getRef(CurrentLogSpans)
-    const minimumLogLevel = fiber.getRef(MinimumLogLevel)
-    if (logLevelGreaterThan(minimumLogLevel, logLevel)) {
+    if (cause === undefined) {
+      cause = causeFromFailures([])
+    }
+    return withFiber((fiber) => {
+      const clock = fiber.getRef(CurrentClock)
+      const loggers = fiber.getRef(CurrentLoggers)
+      const annotations = fiber.getRef(CurrentLogAnnotations)
+      const logLevel = level ?? fiber.getRef(CurrentLogLevel)
+      const spans = fiber.getRef(CurrentLogSpans)
+      const minimumLogLevel = fiber.getRef(MinimumLogLevel)
+      if (logLevelGreaterThan(minimumLogLevel, logLevel)) {
+        return void_
+      }
+      if (loggers.size > 0) {
+        const date = new Date(clock.unsafeCurrentTimeMillis())
+        for (const logger of loggers) {
+          logger.log({
+            annotations,
+            cause,
+            context: fiber.context,
+            date,
+            fiberId: fiber.id,
+            logLevel,
+            message,
+            spans
+          })
+        }
+      }
       return void_
-    }
-    if (loggers.size > 0) {
-      const date = new Date(clock.unsafeCurrentTimeMillis())
-      for (const logger of loggers) {
-        logger.log({
-          annotations,
-          cause,
-          context: fiber.context,
-          date,
-          fiberId: fiber.id,
-          logLevel,
-          message,
-          spans
-        })
-      }
-    }
-    return void_
-  })
-}
-
-/** @internal */
-export const log: (...message: ReadonlyArray<any>) => Effect.Effect<void> = logWithLevel()
-
-/** @internal */
-export const logFatal: (...message: ReadonlyArray<any>) => Effect.Effect<void> = logWithLevel("Fatal")
-
-/** @internal */
-export const logWarning: (...message: ReadonlyArray<any>) => Effect.Effect<void> = logWithLevel("Warning")
-
-/** @internal */
-export const logError: (...message: ReadonlyArray<any>) => Effect.Effect<void> = logWithLevel("Error")
-
-/** @internal */
-export const logInfo: (...message: ReadonlyArray<any>) => Effect.Effect<void> = logWithLevel("Info")
-
-/** @internal */
-export const logDebug: (...message: ReadonlyArray<any>) => Effect.Effect<void> = logWithLevel("Debug")
-
-/** @internal */
-export const logTrace: (...message: ReadonlyArray<any>) => Effect.Effect<void> = logWithLevel("Trace")
+    })
+  }
