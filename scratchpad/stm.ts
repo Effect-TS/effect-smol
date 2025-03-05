6145886380ecc3402ebf318d98b3fcd01ebd4eca
Michael Arnaldi <michael.arnaldi@effectful.co>
2025-03-05 18:19:23 +0100

    improve example

--- scratchpad/stm.ts
+++ scratchpad/stm.ts
@@ -5,24 +5,26 @@ import * as TxRef from "effect/TxRef"
 const program = Effect.gen(function*() {
   const ref1 = yield* TxRef.make(Option.none())
   const ref2 = yield* TxRef.make(Option.none())
-  let attempt = 0
 
-  const somes = Effect.gen(function*() {
+  yield* Effect.gen(function*() {
+    yield* TxRef.set(ref1, Option.some(`a`))
+    yield* TxRef.set(ref2, Option.some(`b`))
+  }).pipe(
+    Effect.transaction,
+    Effect.delay("1 second"),
+    Effect.fork
+  )
+
+  let attempt = 0
+  yield* Effect.gen(function*() {
     const [a, b] = [yield* TxRef.get(ref1), yield* TxRef.get(ref2)]
     yield* Effect.log(`(attempt ${++attempt}) a: ${a._tag}, b: ${b._tag}`)
     if (a._tag === "None" || b._tag === "None") {
       return yield* Effect.retryTransaction
     }
-  }).pipe(Effect.transaction)
-
-  const update = Effect.gen(function*() {
-    yield* TxRef.set(ref1, Option.some(`a`))
-    yield* TxRef.set(ref2, Option.some(`b`))
-  }).pipe(Effect.transaction)
-
-  yield* Effect.fork(update.pipe(Effect.delay("1 second")))
-
-  yield* somes.pipe(Effect.tap(Effect.logDebug))
+  }).pipe(
+    Effect.transaction
+  )
 })
 
 Effect.runPromise(program).catch(console.error)
