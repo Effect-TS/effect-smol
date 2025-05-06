65aea495e5f27793bf4ff7d0473737a2dbc2c5c0
Giulio Canti <giulio.canti@gmail.com>
2025-05-06 09:56:07 +0200

    align SchemaIssue.isIssue with the standard

--- packages/effect/src/SchemaIssue.ts
+++ packages/effect/src/SchemaIssue.ts
@@ -4,18 +4,28 @@
 
 import type * as Arr from "./Array.js"
 import type * as Option from "./Option.js"
+import { hasProperty } from "./Predicate.js"
 import type * as SchemaAST from "./SchemaAST.js"
 import type * as SchemaFilter from "./SchemaFilter.js"
 import type * as SchemaMiddleware from "./SchemaMiddleware.js"
 
 /**
- * @category model
+ * @since 4.0.0
+ * @category Symbols
+ */
+export const TypeId: unique symbol = Symbol.for("effect/SchemaIssue")
+
+/**
+ * @since 4.0.0
+ * @category Symbols
+ */
+export type TypeId = typeof TypeId
+
+/**
  * @since 4.0.0
  */
 export function isIssue(u: unknown): u is Issue {
-  return u instanceof MismatchIssue || u instanceof InvalidIssue || u instanceof MissingIssue ||
-    u instanceof ForbiddenIssue || u instanceof FilterIssue || u instanceof MiddlewareIssue ||
-    u instanceof TransformationIssue || u instanceof PointerIssue || u instanceof CompositeIssue
+  return hasProperty(u, TypeId)
 }
 
 /**
@@ -42,6 +52,7 @@ export type Issue =
  * @since 4.0.0
  */
 export class TransformationIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "TransformationIssue"
   constructor(
     readonly parser: SchemaAST.Transformation["decode"],
@@ -56,6 +67,7 @@ export class TransformationIssue {
  * @since 4.0.0
  */
 export class FilterIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "FilterIssue"
   constructor(
     readonly filter: SchemaFilter.Filters<unknown>,
@@ -71,6 +83,7 @@ export class FilterIssue {
  * @since 4.0.0
  */
 export class MiddlewareIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "MiddlewareIssue"
   constructor(
     readonly middleware: SchemaMiddleware.Middleware<any, any, any, any>,
@@ -91,6 +104,7 @@ export type PropertyKeyPath = ReadonlyArray<PropertyKey>
  * @since 4.0.0
  */
 export class PointerIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "PointerIssue"
   constructor(
     readonly path: PropertyKeyPath,
@@ -105,6 +119,7 @@ export class PointerIssue {
  * @since 4.0.0
  */
 export class MissingIssue {
+  readonly [TypeId] = TypeId
   static readonly instance = new MissingIssue()
   readonly _tag = "MissingIssue"
   private constructor() {}
@@ -117,6 +132,7 @@ export class MissingIssue {
  * @since 4.0.0
  */
 export class CompositeIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "CompositeIssue"
   constructor(
     readonly ast: SchemaAST.AST,
@@ -130,6 +146,7 @@ export class CompositeIssue {
  * @since 4.0.0
  */
 export class MismatchIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "MismatchIssue"
   constructor(
     readonly ast: SchemaAST.AST,
@@ -143,6 +160,7 @@ export class MismatchIssue {
  * @since 4.0.0
  */
 export class InvalidIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "InvalidIssue"
   constructor(
     readonly actual: Option.Option<unknown>,
@@ -157,6 +175,7 @@ export class InvalidIssue {
  * @since 4.0.0
  */
 export class ForbiddenIssue {
+  readonly [TypeId] = TypeId
   readonly _tag = "ForbiddenIssue"
   constructor(
     readonly actual: Option.Option<unknown>,
