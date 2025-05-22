9885712a103b43e5fabec93acc09efae9eb7083b
Giulio Canti <giulio.canti@gmail.com>
2025-05-22 08:43:01 +0200

    fix bundle

--- bundle/schema-string-async.ts
+++ bundle/schema-string-async.ts
@@ -1,6 +1,5 @@
 import * as Duration from "#dist/effect/Duration"
 import * as Effect from "#dist/effect/Effect"
-import * as Option from "#dist/effect/Option"
 import * as Result from "#dist/effect/Result"
 import * as Schema from "#dist/effect/Schema"
 import * as SchemaParser from "#dist/effect/SchemaParser"
@@ -8,14 +7,14 @@ import * as SchemaTransformation from "#dist/effect/SchemaTransformation"
 
 const schema = Schema.String.pipe(Schema.decodeTo(
   Schema.String,
-  SchemaTransformation.transformOrFail(
-    (s) =>
+  SchemaTransformation.transformOrFail({
+    decode: (s) =>
       Effect.gen(function*() {
         yield* Effect.clockWith((clock) => clock.sleep(Duration.millis(300)))
-        return Option.some(s)
+        return s
       }),
-    (s) => Result.succeedSome(s)
-  )
+    encode: Result.ok
+  })
 ))
 
 SchemaParser.decodeUnknown(schema)("a").pipe(Effect.runPromise).then(console.log)
