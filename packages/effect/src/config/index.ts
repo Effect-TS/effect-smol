54d66caff6a7e718db26930874d8e70cb42f398e
Tim <hello@timsmart.co>
2025-07-10 14:53:38 +1200

    docs

--- packages/effect/src/config/index.ts
+++ packages/effect/src/config/index.ts
@@ -3,16 +3,16 @@
  */
 
 /**
- * @since 2.0.0
+ * @since 4.0.0
  */
 export * as Config from "./Config.js"
 
 /**
- * @since 2.0.0
+ * @since 4.0.0
  */
 export * as ConfigError from "./ConfigError.js"
 
 /**
- * @since 2.0.0
+ * @since 4.0.0
  */
 export * as ConfigProvider from "./ConfigProvider.js"
