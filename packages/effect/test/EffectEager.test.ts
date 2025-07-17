5e74d4a27f40d751380d38a17b057e5d799f115b
Sebastian Lorenz <fubhy@fubhy.com>
2025-07-17 11:39:22 +0200

    fix imports

--- packages/effect/test/EffectEager.test.ts
+++ packages/effect/test/EffectEager.test.ts
@@ -1,8 +1,8 @@
 import { assert, describe, it } from "@effect/vitest"
-import * as Effect from "../src/Effect.ts"
-import * as Exit from "../src/Exit.ts"
-import * as Fiber from "../src/Fiber.ts"
-import * as TestClock from "../src/TestClock.ts"
+import * as Effect from "effect/Effect"
+import * as Exit from "effect/Exit"
+import * as Fiber from "effect/Fiber"
+import * as TestClock from "effect/TestClock"
 
 describe("Effect Eager Operations", () => {
   describe("fnUntracedEager", () => {
