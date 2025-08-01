952885f1a78d48e3a848d689d334c9d7d5c4a64b
Tim <hello@timsmart.co>
2025-08-01 23:29:23 +1200

    wip

--- packages/effect/src/stream/Channel.ts
+++ packages/effect/src/stream/Channel.ts
@@ -3659,25 +3659,20 @@ export const pipeToOrFail: {
 export const unwrap = <OutElem, OutErr, OutDone, InElem, InErr, InDone, R2, E, R>(
   channel: Effect.Effect<Channel<OutElem, OutErr, OutDone, InElem, InErr, InDone, R2>, E, R>
 ): Channel<OutElem, E | OutErr, OutDone, InElem, InErr, InDone, Exclude<R, Scope.Scope> | R2> =>
-  fromTransform((upstream, scope) =>
-    channel.pipe(
-      Scope.provide(scope),
-      Effect.flatMap((channel) => toTransform(channel)(upstream, scope)),
-      Effect.forkIn(scope),
-      Effect.map((fiber) => {
-        let pull: Pull.Pull<OutElem, E | OutErr, OutDone> | undefined
-        return Effect.suspend(() => {
-          if (pull) return pull
-          return Fiber.join(fiber).pipe(
-            Effect.flatMap((pull_) => {
-              pull = pull_
-              return pull_
-            })
-          )
+  fromTransform((upstream, scope) => {
+    let pull: Pull.Pull<OutElem, E | OutErr, OutDone> | undefined
+    return Effect.succeed(Effect.suspend(() => {
+      if (pull) return pull
+      return channel.pipe(
+        Scope.provide(scope),
+        Effect.flatMap((channel) => toTransform(channel)(upstream, scope)),
+        Effect.flatMap((pull_) => {
+          pull = pull_
+          return pull_
         })
-      })
-    )
-  )
+      )
+    }))
+  })
 
 /**
  * Returns a new channel which embeds the given input handler into a Channel.
