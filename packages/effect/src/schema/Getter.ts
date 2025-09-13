23f9e259cb09a32f997c4eefda66276a0121da22
Giulio Canti <giulio.canti@gmail.com>
2025-09-13 15:28:43 +0200

    improve perfs

--- packages/effect/src/schema/Getter.ts
+++ packages/effect/src/schema/Getter.ts
@@ -521,3 +521,11 @@ export function dateTimeUtcFromInput<E extends DateTime.DateTime.Input>(): Gette
       : Effect.fail(new Issue.InvalidValue(Option.some(input), { message: "Invalid DateTime input" }))
   })
 }
+
+/**
+ * @category Constructors
+ * @since 4.0.0
+ */
+export function shallowCopy<E extends object>(): Getter<unknown, E> {
+  return transform((s) => Array.isArray(s) ? s.slice() : { ...s })
+}
