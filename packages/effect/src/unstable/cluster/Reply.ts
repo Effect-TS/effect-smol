7b7a1130f2c944c2b67c3bc4f2698ae6d89f7ab0
Giulio Canti <giulio.canti@gmail.com>
2025-09-18 11:57:16 +0200

    improve declareConstructor definitions

--- packages/effect/src/unstable/cluster/Reply.ts
+++ packages/effect/src/unstable/cluster/Reply.ts
@@ -192,7 +192,7 @@ export class Chunk<R extends Rpc.Any> extends Data.TaggedClass("Chunk")<{
       })
     }, {
       title: "Chunk",
-      defaultIsoSerializer: ([success]) =>
+      defaultJsonSerializer: ([success]) =>
         Schema.link<Chunk<Rpc.Any>>()(
           Schema.Struct({
             _tag: Schema.Literal("Chunk"),
@@ -274,7 +274,7 @@ export class WithExit<R extends Rpc.Any> extends Data.TaggedClass("WithExit")<{
       })
     }, {
       title: "WithExit",
-      defaultIsoSerializer: ([exit]) =>
+      defaultJsonSerializer: ([exit]) =>
         Schema.link<WithExit<Rpc.Any>>()(
           Schema.Struct({
             _tag: Schema.Literal("WithExit"),
