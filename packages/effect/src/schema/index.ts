a178067fe604e21a0a61eb051c8fcc7158419dbd
Tim Smart <hello@timsmart.co>
2025-12-05 09:56:14 +1300

    fix

--- packages/effect/src/schema/index.ts
+++ packages/effect/src/schema/index.ts
@@ -53,14 +53,9 @@ export * as Parser from "./Parser.ts"
 export * as Schema from "./Schema.ts"
 
 /**
-   * The target version of the generated JSON Schema.
-   *
-   * It is *strongly recommended* that implementers support `"draft-2020-12"` and `"draft-07"`, as they are both in wide use.
-   *
-   * The `"openapi-3.0"` target is intended as a standardized specifier for OpenAPI 3.0 which is a superset of JSON Schema `"draft-04"`.
-   *
-   * All other targets can be implemented on a best-effort basis. Libraries should throw if they don't support a specified target.
-   */
+ * @since 4.0.0
+ * @experimental
+ */
 export * as StandardSchema from "./StandardSchema.ts"
 
 /**
