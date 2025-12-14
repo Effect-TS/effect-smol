b396ddf14bbef0a1b1951b3f831ad3f9215514c7
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-12-14 07:37:31 -0500

    add Permissions

--- packages/platform-browser/src/index.ts
+++ packages/platform-browser/src/index.ts
@@ -41,3 +41,8 @@ export * as Clipboard from "./Clipboard.ts"
  * @since 4.0.0
  */
 export * as Geolocation from "./Geolocation.ts"
+
+/**
+ * @since 4.0.0
+ */
+export * as Permissions from "./Permissions.ts"
