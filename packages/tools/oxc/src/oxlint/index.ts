8a770b98e82ffb5e4eaf875401dccbd7d83ac682
Sebastian Lorenz <fubhy@fubhy.com>
2026-01-08 19:08:22 +0100

    chore: add rule to enforce import style

--- packages/tools/oxc/src/oxlint/index.ts
+++ packages/tools/oxc/src/oxlint/index.ts
@@ -1,6 +1,7 @@
 import noImportFromBarrelPackage from "./rules/no-import-from-barrel-package.ts"
 import noJsExtensionImports from "./rules/no-js-extension-imports.ts"
 import noOpaqueInstanceFields from "./rules/no-opaque-instance-fields.ts"
+import preferTopLevelTypeImport from "./rules/prefer-top-level-type-import.ts"
 
 export default {
   meta: {
@@ -9,6 +10,7 @@ export default {
   rules: {
     "no-import-from-barrel-package": noImportFromBarrelPackage,
     "no-js-extension-imports": noJsExtensionImports,
-    "no-opaque-instance-fields": noOpaqueInstanceFields
+    "no-opaque-instance-fields": noOpaqueInstanceFields,
+    "prefer-top-level-type-import": preferTopLevelTypeImport
   }
 }
