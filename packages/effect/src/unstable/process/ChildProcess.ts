ee49f896871ff87f36dae3d3d58d127a5b27677e
Tim Smart <hello@timsmart.co>
2026-01-22 21:51:18 +0000

    EFF-265 add ChildProcess.prefix

--- packages/effect/src/unstable/process/ChildProcess.ts
+++ packages/effect/src/unstable/process/ChildProcess.ts
@@ -567,6 +567,30 @@ const makePipedCommand = (
     options
   })
 
+type PrefixSpec = {
+  readonly command: string
+  readonly args: ReadonlyArray<string>
+}
+
+const parsePrefixSpec = (args: ReadonlyArray<unknown>): PrefixSpec => {
+  if (isTemplateString(args[0])) {
+    const [templates, ...expressions] = args as [TemplateStringsArray, ...ReadonlyArray<TemplateExpression>]
+    const tokens = parseTemplates(templates, expressions)
+    return { command: tokens[0] ?? "", args: tokens.slice(1) }
+  }
+
+  if (isCommand(args[0]) && isStandardCommand(args[0])) {
+    return { command: args[0].command, args: args[0].args }
+  }
+
+  if (typeof args[0] === "string") {
+    const [command, cmdArgs = []] = args as [string, ReadonlyArray<string>?]
+    return { command, args: cmdArgs }
+  }
+
+  throw new Error("Invalid prefix arguments")
+}
+
 /**
  * Create a command from a template literal, options + template, or array form.
  *
@@ -688,6 +712,41 @@ export const pipeTo: {
 )
 
 /**
+ * Prefix a command with another command.
+ *
+ * For pipelines, only the leftmost command is prefixed.
+ *
+ * @example
+ * ```ts
+ * import { ChildProcess } from "effect/unstable/process"
+ *
+ * const command = ChildProcess.make`echo "foo"`
+ *
+ * const prefixed = command.pipe(
+ *   ChildProcess.prefix`time`
+ * )
+ *
+ * // now prefixed will execute `time echo "foo"`
+ * ```
+ *
+ * @since 4.0.0
+ * @category Combinators
+ */
+export const prefix: {
+  (command: string, args?: ReadonlyArray<string>): (self: Command) => Command
+  (templates: TemplateStringsArray, ...expressions: ReadonlyArray<TemplateExpression>): (self: Command) => Command
+  (self: Command, command: string, args?: ReadonlyArray<string>): Command
+} = function prefix(...args: Array<unknown>): any {
+  if (isCommand(args[0]) && args.length > 1) {
+    const [self, ...rest] = args as [Command, ...Array<unknown>]
+    const prefixSpec = parsePrefixSpec(rest)
+    return applyPrefix(self, prefixSpec)
+  }
+  const prefixSpec = parsePrefixSpec(args)
+  return (self: Command): Command => applyPrefix(self, prefixSpec)
+}
+
+/**
  * Set the current working directory for a command.
  *
  * For pipelines, applies to each command in the pipeline.
@@ -720,6 +779,21 @@ export const setCwd: {
     }
   }
 )
+
+const applyPrefix = (self: Command, prefixSpec: PrefixSpec): Command => {
+  switch (self._tag) {
+    case "StandardCommand": {
+      return makeStandardCommand(
+        prefixSpec.command,
+        [...prefixSpec.args, self.command, ...self.args],
+        self.options
+      )
+    }
+    case "PipedCommand": {
+      return makePipedCommand(applyPrefix(self.left, prefixSpec), self.right, self.options)
+    }
+  }
+}
 
 /**
  * Spawn a command and return a handle for interaction.
