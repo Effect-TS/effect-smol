c9b5a6738fe944264fee9e05e2d9af8d9d728b92
ericszentivanyi <247076009+ericszentivanyi@users.noreply.github.com>
2026-01-23 18:05:26 -0500

    feat: port `securitySetCookie` function

--- packages/effect/src/internal/redacted.ts
+++ packages/effect/src/internal/redacted.ts
@@ -1,4 +1,8 @@
-import type * as Redacted from "../Redacted.ts"
+import * as Redacted from "../Redacted.ts"
 
 /** @internal */
 export const redactedRegistry = new WeakMap<Redacted.Redacted<any>, any>()
+
+/** @internal */
+export const stringOrRedacted = (value: string | Redacted.Redacted): string =>
+  typeof value === "string" ? value : Redacted.value(value)
