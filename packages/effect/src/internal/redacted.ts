d2c13a82e912522189bbd8001872943d52fddcab
ericszentivanyi <247076009+ericszentivanyi@users.noreply.github.com>
2026-01-24 10:20:36 -0500

    fix: circular dependency

--- packages/effect/src/internal/redacted.ts
+++ packages/effect/src/internal/redacted.ts
@@ -1,8 +1,19 @@
-import * as Redacted from "../Redacted.ts"
+import type * as Redacted from "../Redacted.ts"
 
 /** @internal */
 export const redactedRegistry = new WeakMap<Redacted.Redacted<any>, any>()
 
 /** @internal */
-export const stringOrRedacted = (value: string | Redacted.Redacted): string =>
-  typeof value === "string" ? value : Redacted.value(value)
+export const value = <T>(self: Redacted.Redacted<T>): T => {
+  if (redactedRegistry.has(self)) {
+    return redactedRegistry.get(self)
+  } else {
+    throw new Error(
+      "Unable to get redacted value" +
+        (self.label ? ` with label: "${self.label}"` : "")
+    )
+  }
+}
+
+/** @internal */
+export const stringOrRedacted = (val: string | Redacted.Redacted): string => typeof val === "string" ? val : value(val)
