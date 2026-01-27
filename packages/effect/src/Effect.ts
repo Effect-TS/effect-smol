e4b1e6a38b3a46c25ee3ead97ade9ba40a63490f
Tim <hello@timsmart.co>
2026-01-27 21:24:57 +1300

    EFF-324: add Effect.request example (#13)
    * docs(effect): add request example
    
    * docs(effect): align request example usage

--- packages/effect/src/Effect.ts
+++ packages/effect/src/Effect.ts
@@ -7595,6 +7595,31 @@ export const withParentSpan: {
 /**
  * Executes a request using the provided resolver.
  *
+ * @example
+ * ```ts
+ * import { Effect, Exit, Request, RequestResolver } from "effect"
+ *
+ * interface GetUser extends Request.Request<string, never> {
+ *   readonly _tag: "GetUser"
+ *   readonly id: number
+ * }
+ *
+ * const GetUser = Request.tagged<GetUser>("GetUser")
+ *
+ * const UserResolver = RequestResolver.make<GetUser>((entries) =>
+ *   Effect.sync(() => {
+ *     for (const entry of entries) {
+ *       entry.completeUnsafe(Exit.succeed(`user-${entry.request.id}`))
+ *     }
+ *   })
+ * )
+ *
+ * const program = Effect.gen(function*() {
+ *   const user = yield* Effect.request(GetUser({ id: 1 }), UserResolver)
+ *   return user
+ * })
+ * ```
+ *
  * @since 2.0.0
  * @category requests & batching
  */
