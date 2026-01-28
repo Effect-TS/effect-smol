07697673f75ca4ef4c1f4e1c430caa85dc6f9ade
Kit Langton <kit.langton@gmail.com>
2026-01-28 07:52:06 -0500

    TIM-259 Improve Stream.tapSink JSDoc

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -2062,8 +2062,31 @@ export const tapBoth: {
  * Sends all elements emitted by this stream to the specified sink in addition
  * to emitting them.
  *
+ * @example
+ * ```ts
+ * import { Console, Effect, Ref, Sink, Stream } from "effect"
+ *
+ * const program = Effect.gen(function*() {
+ *   const seen = yield* Ref.make<Array<number>>([])
+ *   const sink = Sink.forEach((value: number) =>
+ *     Ref.update(seen, (items) => [...items, value])
+ *   )
+ *   const result = yield* Stream.make(1, 2, 3).pipe(
+ *     Stream.tapSink(sink),
+ *     Stream.runCollect
+ *   )
+ *   const tapped = yield* Ref.get(seen)
+ *   yield* Console.log(tapped)
+ *   yield* Console.log(result)
+ * })
+ *
+ * Effect.runPromise(program)
+ * // Output: [1, 2, 3]
+ * // Output: [1, 2, 3]
+ * ```
+ *
  * @since 2.0.0
- * @category sequencing
+ * @category Sequencing
  */
 export const tapSink: {
   <A, E2, R2>(sink: Sink.Sink<unknown, A, unknown, E2, R2>): <E, R>(self: Stream<A, E, R>) => Stream<A, E2 | E, R2 | R>
