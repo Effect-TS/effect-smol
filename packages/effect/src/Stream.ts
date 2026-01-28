39254e6ddd6c26e36ea818b3308b8d5d87e57f47
Tim <hello@timsmart.co>
2026-01-28 15:43:02 +1300

    docs: update Stream.accumulate JSDoc (TIM-368) (#153)

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -5817,8 +5817,28 @@ export const pipeThrough: {
 export const collect = <A, E, R>(self: Stream<A, E, R>): Stream<Array<A>, E, R> => fromEffect(runCollect(self))
 
 /**
+ * Accumulates elements into a growing array, emitting the cumulative array for each input chunk.
+ *
+ * @example
+ * ```ts
+ * import { Console, Effect, Stream } from "effect"
+ *
+ * const program = Effect.gen(function*() {
+ *   const accumulated = yield* Stream.runCollect(
+ *     Stream.fromArray([1, 2, 3]).pipe(
+ *       Stream.rechunk(1),
+ *       Stream.accumulate
+ *     )
+ *   )
+ *   yield* Console.log(accumulated)
+ * })
+ *
+ * Effect.runPromise(program)
+ * //=> { _id: 'Chunk', values: [ [ 1 ], [ 1, 2 ], [ 1, 2, 3 ] ] }
+ * ```
+ *
  * @since 2.0.0
- * @category accumulation
+ * @category Accumulation
  */
 export const accumulate = <A, E, R>(self: Stream<A, E, R>): Stream<Arr.NonEmptyArray<A>, E, R> =>
   mapAccumArray(self, Arr.empty<A>, (acc, as) => {
