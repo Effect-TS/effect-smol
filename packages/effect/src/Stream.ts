02df835c3ef7ea7e5187de0f6ee311d18a885382
Tim <hello@timsmart.co>
2026-01-28 15:14:22 +1300

    docs: improve interleaveWith JSDoc (TIM-378) (#143)

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -6120,34 +6120,33 @@ export const interleave: {
 )
 
 /**
- * Combines this stream and the specified stream deterministically using the
- * stream of boolean values `pull` to control which stream to pull from next.
- * A value of `true` indicates to pull from this stream and a value of `false`
- * indicates to pull from the specified stream. Only consumes as many elements
- * as requested by the `pull` stream. If either this stream or the specified
- * stream are exhausted further requests for values from that stream will be
- * ignored.
+ * Interleaves two streams deterministically by following a boolean decider stream.
+ *
+ * The decider controls how many elements are pulled; if one side ends, pulls for
+ * that side are ignored.
  *
  * @example
  * ```ts
- * import { Effect, Stream } from "effect"
+ * import { Console, Effect, Stream } from "effect"
  *
- * const s1 = Stream.make(1, 3, 5, 7, 9)
- * const s2 = Stream.make(2, 4, 6, 8, 10)
+ * const program = Effect.gen(function*() {
+ *   const left = Stream.make(1, 3, 5)
+ *   const right = Stream.make(2, 4, 6)
+ *   const decider = Stream.make(true, false, false, true, true)
  *
- * const booleanStream = Stream.make(true, false, false).pipe(Stream.forever)
+ *   const values = yield* Stream.runCollect(
+ *     Stream.interleaveWith(left, right, decider)
+ *   )
  *
- * const stream = Stream.interleaveWith(s1, s2, booleanStream)
+ *   yield* Console.log(values)
+ * })
  *
- * Effect.runPromise(Stream.runCollect(stream)).then(console.log)
- * // [
- * //   1, 2,  4, 3, 6,
- * //   8, 5, 10, 7, 9
- * // ]
+ * Effect.runPromise(program)
+ * // [ 1, 2, 4, 3, 5 ]
  * ```
  *
  * @since 2.0.0
- * @category utils
+ * @category Utils
  */
 export const interleaveWith: {
   <A2, E2, R2, E3, R3>(
