82131dd9117a113430c95423d6d68a7248f72680
Michael Arnaldi <michael.arnaldi@effectful.co>
2026-03-31 21:22:33 +0200

    add unstable machine prototype

---
+++ packages/effect/dtslint/unstable/machine/Machine.tst.ts
@@ -0,0 +1,138 @@
+import { Schema } from "effect"
+import * as Machine from "effect/unstable/machine/Machine"
+import { describe, expect, it } from "tstyche"
+
+describe("Machine", () => {
+  it("infers machine state schemas from state definitions", () => {
+    const User = Schema.Struct({
+      id: Schema.String,
+      email: Schema.String
+    })
+
+    class Create extends Schema.TaggedClass<Create, { readonly _: unique symbol }>()(
+      "Create",
+      { email: Schema.String }
+    ) {}
+
+    class Rename extends Schema.TaggedClass<Rename, { readonly _: unique symbol }>()(
+      "Rename",
+      { email: Schema.String }
+    ) {}
+
+    class Delete extends Schema.TaggedClass<Delete, { readonly _: unique symbol }>()(
+      "Delete",
+      {}
+    ) {}
+
+    class Uncreated extends Schema.TaggedClass<Uncreated, { readonly _: unique symbol }>()(
+      "Uncreated",
+      {}
+    ) {}
+
+    class Created extends Schema.TaggedClass<Created, { readonly _: unique symbol }>()(
+      "Created",
+      { user: User }
+    ) {}
+
+    class Deleted extends Schema.TaggedClass<Deleted, { readonly _: unique symbol }>()(
+      "Deleted",
+      { userId: Schema.String }
+    ) {}
+
+    const UserMachine = Machine.make({
+      events: [Create, Rename, Delete],
+      initial: new Uncreated({}),
+      states: [Uncreated, Created, Deleted]
+    }).handlers({
+      Uncreated: {
+        Create: ({ event }) => new Created({ user: { id: "user-1", email: event.email } })
+      },
+      Created: {
+        Rename: ({ data, event }) => new Created({ user: { ...data.user, email: event.email } }),
+        Delete: ({ data }) => new Deleted({ userId: data.user.id })
+      }
+    })
+
+    expect<Machine.Snapshot<Machine.StateSchemasOf<typeof UserMachine>>>().type.toBe<
+      | Uncreated
+      | Created
+      | Deleted
+    >()
+  })
+
+  it("contextually types handlers", () => {
+    class Create extends Schema.TaggedClass<Create, { readonly _: unique symbol }>()(
+      "Create",
+      { email: Schema.String }
+    ) {}
+
+    class Uncreated extends Schema.TaggedClass<Uncreated, { readonly _: unique symbol }>()(
+      "Uncreated",
+      { count: Schema.Number }
+    ) {}
+
+    class Created extends Schema.TaggedClass<Created, { readonly _: unique symbol }>()(
+      "Created",
+      { email: Schema.String }
+    ) {}
+
+    Machine.make({
+      events: [Create],
+      initial: new Uncreated({ count: 0 }),
+      states: [Uncreated, Created]
+    }).handlers({
+      Uncreated: {
+        Create: ({ event, data, snapshot }) => {
+          expect<typeof event>().type.toBe<Create>()
+          expect<typeof data>().type.toBe<{ readonly count: number }>()
+          expect<typeof snapshot>().type.toBe<Uncreated>()
+          return new Created({ email: event.email })
+        }
+      }
+    })
+  })
+
+  it("accepts events as a tuple of tagged schemas", () => {
+    class Create extends Schema.TaggedClass<Create, { readonly _: unique symbol }>()(
+      "Create",
+      { email: Schema.String }
+    ) {}
+
+    class Rename extends Schema.TaggedClass<Rename, { readonly _: unique symbol }>()(
+      "Rename",
+      { email: Schema.String }
+    ) {}
+
+    class Idle extends Schema.TaggedClass<Idle, { readonly _: unique symbol }>()(
+      "Idle",
+      {}
+    ) {}
+
+    class Running extends Schema.TaggedClass<Running, { readonly _: unique symbol }>()(
+      "Running",
+      { email: Schema.String }
+    ) {}
+
+    const machine = Machine.make({
+      events: [Create, Rename],
+      initial: new Idle({}),
+      states: [Idle, Running]
+    }).handlers({
+      Idle: {
+        Create: ({ event }) => {
+          expect<typeof event>().type.toBe<Create>()
+          return new Running({ email: event.email })
+        }
+      },
+      Running: {
+        Rename: ({ event }) => {
+          expect<typeof event>().type.toBe<Rename>()
+          return new Running({ email: event.email })
+        }
+      }
+    })
+
+    expect<Machine.Event<typeof machine.event>>().type.toBe<Create | Rename>()
+    expect<Machine.MachineErrorOf<typeof machine>>().type.toBe<Machine.UnhandledEventError>()
+  })
+})
