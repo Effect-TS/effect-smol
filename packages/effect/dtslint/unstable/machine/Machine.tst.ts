c7491a1146e6bb23cfe734680e3f5d3646659102
Michael Arnaldi <michael.arnaldi@effectful.co>
2026-04-03 21:54:58 +0200

    fix machine dtslint schema import

--- packages/effect/dtslint/unstable/machine/Machine.tst.ts
+++ packages/effect/dtslint/unstable/machine/Machine.tst.ts
@@ -1,4 +1,4 @@
-import { Schema } from "effect"
+import * as Schema from "effect/Schema"
 import * as Machine from "effect/unstable/machine/Machine"
 import { describe, expect, it } from "tstyche"
 
