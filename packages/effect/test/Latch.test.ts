bc95f227e37dba5e08f1b7851fad85d48a02a2b1
Tim Smart <hello@timsmart.co>
2026-04-22 11:11:27 +1200

    fix Latch.release

--- packages/effect/test/Latch.test.ts
+++ packages/effect/test/Latch.test.ts
@@ -44,4 +44,21 @@ describe("Latch", () => {
 
       assert.isTrue(done)
     }))
+
+  it.effect("release lets waiters through", () =>
+    Effect.gen(function*() {
+      const latch = yield* Latch.make(false)
+      const waiter = yield* Effect.forkChild(
+        Latch.await(latch),
+        { startImmediately: true }
+      )
+
+      assert.isUndefined(waiter.pollUnsafe())
+
+      yield* latch.release
+      yield* Effect.yieldNow
+      yield* Effect.yieldNow
+
+      assert.isDefined(waiter.pollUnsafe())
+    }))
 })
