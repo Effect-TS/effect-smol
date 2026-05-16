d96025538ca340439798d4cdbc3287b21ec2d212
juemrami <juemrami@gmail.com>
2026-05-15 21:45:39 -0700

    feat: add indexedDb backed KeyValueStore layer

--- packages/platform-browser/test/BrowserKeyValueStore.test.ts
+++ packages/platform-browser/test/BrowserKeyValueStore.test.ts
@@ -1,7 +1,13 @@
 import * as BrowserKeyValueStore from "@effect/platform-browser/BrowserKeyValueStore"
 import { describe } from "@effect/vitest"
 import { testLayer } from "effect-test/unstable/persistence/KeyValueStore.test"
+import { indexedDB as fakeIndexedDb } from "fake-indexeddb"
 
 describe("KeyValueStore / layerLocalStorage", () => testLayer(BrowserKeyValueStore.layerLocalStorage))
 
 describe("KeyValueStore / layerSessionStorage", () => testLayer(BrowserKeyValueStore.layerSessionStorage))
+
+describe("KeyValueStore / layerIndexedDb", () => {
+  Reflect.set(globalThis, "indexedDB", fakeIndexedDb)
+  testLayer(BrowserKeyValueStore.layerIndexedDb({ database: "kvs_test_db" }))
+})
